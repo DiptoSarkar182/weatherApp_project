1c366686a77d41d9611bab4dd66adda6684862ce
DiptoSarkar182 <diptomanchester2018@gmail.com>
2023-10-03 22:40:35 +0600

    only added place, date, time, day. no css was done.

--- src/fetchAndShowWeather.js
+++ src/fetchAndShowWeather.js
@@ -2,86 +2,71 @@ function initialize(){
     const showData = document.querySelector('.showData');
     const searchBtn = document.querySelector('.searchBtn');
     const searchArea = document.getElementById('searchArea');
+    const searchError = document.querySelector('#searchArea + span.error');
+    const cityAndCountry = document.getElementById('cityAndCountry');
+    const showDate = document.getElementById('date');
+    const showTime = document.getElementById('time');
+    const showDay = document.getElementById('week');
 
     searchBtn.addEventListener('click', (event)=>{
         event.preventDefault();
-        showData.textContent = '';
-        const searchValue = String(searchArea.value);
-        localStorage.setItem('myValue',searchValue);
-        fetchAndShowWeatherData();
+        if(searchArea.value === ''){
+            searchError.textContent = 'Please enter a location';
+        }
+        else {
+            const searchValue = String(searchArea.value);
+            fetchAndShowWeatherData(searchValue);
+        }
     });
 
-    async function fetchAndShowWeatherData(){
-        const api_key = `http://api.weatherapi.com/v1/current.json?key=02cd537bb4c044b6bc9163059232909&q=${localStorage.getItem('myValue')}&aqi=yes`;
+    async function fetchAndShowWeatherData(getValue){
+        searchError.textContent = '';
+        const api_key = `http://api.weatherapi.com/v1/current.json?key=b5e7182569e7426eb6e160345230310&q=${getValue}&aqi=yes`;
         try{
-            const response = await fetch(api_key,{mode:'cors'})
+            const response = await fetch(api_key, {mode:'cors'})
             
             if(!response.ok){
-                throw new Error('response was not ok!!!');
+                searchError.textContent = 'Location not found!'; 
             }
 
             const fetchData = await response.json();
-            const infoArray = [];
 
-            if(fetchData.location.name){
-                const cityName = fetchData.location.name;
-                infoArray.push(String(cityName));
-            }
-
-            if(fetchData.location.country){
-                const countryName = fetchData.location.country;
-                infoArray.push(String(countryName));
+            if(fetchData.location.name && fetchData.location.country){
+                const cityName = String(fetchData.location.name);
+                const countryName = String(fetchData.location.country);
+                cityAndCountry.textContent = `${cityName}, ${countryName}`; 
             }
 
             if(fetchData.location.localtime){
-                const localTime = fetchData.location.localtime;
-                infoArray.push(localTime);
+                const localtimeAndDate = fetchData.location.localtime;
+                const date = new Date(localtimeAndDate);
+                
+                const year = date.getFullYear();
+                const month = date.getMonth() + 1;
+                const day = date.getDate();
+                const hours = date.getHours();
+                const minutes = date.getMinutes();
+                const amOrPm = hours >=12 ? "PM" : "AM"
+                const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thus', 'Fri', 'Sat'];
+                const weekdaysIndex = date.getDay();
+                const weekday = weekdays[weekdaysIndex];
+                showDate.textContent = `${day}/${month}/${year}`;
+                if(minutes<10){
+                    showTime.textContent = `${hours % 12 || 12}:0${minutes} ${amOrPm}`; //12hour conversion
+                }
+                else{
+                    showTime.textContent = `${hours % 12 || 12}:${minutes} ${amOrPm}`; //12hour conversion
+                }
+                showDay.textContent = `${weekday}`;
+
             }
 
-            if(fetchData.current.temp_c){
-                const tempC = fetchData.current.temp_c;
-                infoArray.push(tempC);
-            }
 
-            if(fetchData.current.temp_f){
-                const tempF = fetchData.current.temp_f;
-                infoArray.push(tempF);
-            }
-
-            if(fetchData.current.condition.text){
-                const weatherCondition = fetchData.current.condition.text;
-                infoArray.push(weatherCondition);
-            }
-
-            if(fetchData.current.condition.icon){
-                const weatherIcon = fetchData.current.condition.icon;
-                infoArray.push(weatherIcon);
-            }
-
-            if(fetchData.current.wind_mph){
-                const windMPH = fetchData.current.wind_mph;
-                infoArray.push(windMPH);
-            }
-
-            if(fetchData.current.wind_kph){
-                const windKPH = fetchData.current.wind_kph;
-                infoArray.push(windKPH);
-            }
-
-            if(fetchData.current.humidity){
-                const humidity = fetchData.current.humidity;
-                infoArray.push(humidity);
-            }
-
-            infoArray.forEach((value)=>{
-                const paragraph = document.createElement("p");
-                paragraph.textContent = value;
-                showData.appendChild(paragraph);
-            });
 
         }catch(error){
             console.error(error);
         }
     }
+
 }
 export default initialize;
