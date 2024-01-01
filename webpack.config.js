0cc2bb68d6cd482ba78d60f08053db4a18bf91c7
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-01-01 21:56:30 +0600

    added .env

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new Dotenv()
+  ]
 };
